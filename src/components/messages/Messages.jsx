e924a9178c0cef5aa63950ed27ced729e72af9be
Akash <kumawat.aakash515@gmail.com>
2024-02-16 16:01:13 +0530

    get message functionality done

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,6 +1,7 @@
 import Message from "./Message.jsx"
-
+import useGetConversations from "../../hooks/useGetConversations.js";
 const Messages = () => {
+  useGetConversations();
   return (
     <div className="px-4 flex-1 overflow-auto">
 
