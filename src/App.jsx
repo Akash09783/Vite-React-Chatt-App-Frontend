5a2aca7aaeff86f79f99ff72534e06a0467dfe54
Akash <kumawat.aakash515@gmail.com>
2024-02-16 12:32:13 +0530

    UI Design Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 
 import './App.css'
+import Home from './pages/home/Home.jsx'
 // import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
+// import SignUp from './pages/signup/SignUp'
 
 function App() {
 
 
-  return <div className=' p-4 h-screen flex-items-center justify-center'>
-    <SignUp/>
+  return <div className='p-4 h-screen flex items-center justify-center'>
+    <Home/>
   </div>
 
 }
