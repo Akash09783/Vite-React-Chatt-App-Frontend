54f04304337ce17e4cecaa5a34d6f482b59b2a1c
Akash <kumawat.aakash515@gmail.com>
2024-02-16 15:09:42 +0530

    login and logout button integration done

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@
 import './App.css'
 import Home from './pages/home/Home.jsx'
 import { Navigate, Route, Routes} from 'react-router-dom';
-import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext.jsx';
 
@@ -14,9 +14,9 @@ const{authUser} = useAuthContext();
   return <div className='p-4 h-screen flex items-center justify-center'>
 
 <Routes>
-  <Route path='/' element={<Home/>}   />
-  <Route path='/login' element={<Login/>}   />
-  <Route path='/signup' element={authUser ? <Navigate to='/'/>:<SignUp/>}   />
+<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 
 </Routes>
 
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
-import useSignup from "../../hooks/useSignup.js";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
 	const [inputs, setInputs] = useState({
@@ -27,7 +27,7 @@ const SignUp = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up <span className='text-blue-500'> ChatApp</span>
+					Sign Up <span className='text-blue-500'> Roomie</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
@@ -37,7 +37,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='John Doe'
+							placeholder='Enter Full Name'
 							className='w-full input input-bordered  h-10'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
@@ -50,7 +50,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='johndoe'
+							placeholder='Enter Username'
 							className='w-full input input-bordered h-10'
 							value={inputs.username}
 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
@@ -62,7 +62,8 @@ const SignUp = () => {
 							<span className='text-base label-text'>Password</span>
 						</label>
 						<input
-							type='password'
+							type='current-password'
+              
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
 							value={inputs.password}
@@ -75,9 +76,10 @@ const SignUp = () => {
 							<span className='text-base label-text'>Confirm Password</span>
 						</label>
 						<input
+            
 							type='password'
 							placeholder='Confirm Password'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-10 '
 							value={inputs.confirmPassword}
 							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
 						/>
@@ -88,7 +90,7 @@ const SignUp = () => {
 					<Link
 						to={"/login"}
 						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
-						href='#'
+					
 					>
 						Already have an account?
 					</Link>
